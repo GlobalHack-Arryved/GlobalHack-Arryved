cba08dd0613d947f7d77dffe1b1b35541de80ec3
Jesse Dunlap <jesse@zipline.co>
2018-10-13 18:26:39 -0500

    Added heading and text to categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import { Icon, Spinner } from '@blueprintjs/core';
-import { PageContainer } from '../Common/StyledComponents';
-import { TopButton } from './CategoriesComponents';
+import { TopButton, CategoriesWrapper } from './CategoriesComponents';
 
 class Categories extends Component {
   render() {
     const { t } = this.props;
 
     return (
-      <PageContainer>
-        {
-          this.props.categories.data.length
-            ? this.props.categories.data.map((c, index) =>
-              <TopButton
-                first={index === 0}
-                key={c.id}
-                onClick={(e) => {
-                  this.props.onCategoryPress(e, c);
-                }}
-              >
-                <Icon icon={c.icon} iconSize={20} />
-                <p>{t(c.name)}</p>
-              </TopButton>
-            )
-            : <Spinner />
-        }
-      </PageContainer>
+      <CategoriesWrapper>
+        <h1>
+          St. Louis, MO
+        </h1>
+
+        <p className="bp3-text-large bp3-text-muted">
+          {t('Select your desired category to find what you need.')}
+        </p>
+
+        {this.props.categories.data.map((c, index) =>
+          <TopButton
+            first={index === 0}
+            key={c.id}
+            onClick={(e) => {
+              this.props.onCategoryPress(e, c);
+            }}
+          >
+            <Icon icon={c.icon} iconSize={20} />
+            <p>{t(c.name)}</p>
+          </TopButton>
+        )}
+      </CategoriesWrapper>
     );
   }
 }
--- src/components/Categories/CategoriesComponents.js
+++ src/components/Categories/CategoriesComponents.js
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
+import { Colors } from '@blueprintjs/core';
+
+export const CategoriesWrapper = styled.div`
+  padding: 30px;
+
+  h1 {
+    margin: 40px 0 10px 0;
+    color: ${Colors.BLUE2};
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+`;
 
 export const TopButton = styled.button`
   background-color: #3C86B5;
@@ -8,7 +22,7 @@ export const TopButton = styled.button`
   padding: 0 30px;
   border-radius: 3px;
   border: 0;
-  margin: ${props => props.first ? `12px 0` : `0 0 12px 0`}
+  margin: ${props => props.first ? `12px 0` : `0 0 12px 0`};
   box-shadow: 0 0 0 1px rgba(16,22,26,0.1), 0 1px 1px 0 rgba(16,22,26,0.2);
   display: flex;
   flex-direction: row;
@@ -24,7 +38,6 @@ export const TopButton = styled.button`
     margin: 0;
     font-size: 15px;
     font-weight: 600;
-    font-family: "SF Pro Text", sans-serif;
   }
 
   span {
