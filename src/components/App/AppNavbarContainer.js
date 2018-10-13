814092a76bf042aa1d0d6846e8608ba18174a657
Jesse Dunlap <jesse@zipline.co>
2018-10-13 01:37:53 -0500

    Converted Navbar to use localization

--- src/components/App/AppNavbarContainer.js
+++ src/components/App/AppNavbarContainer.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase';
+import { withNamespaces } from 'react-i18next';
 import { compose, withHandlers } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import AppNavbar from './AppNavbar';
 
 export default compose(
+  withNamespaces(),
   withRouter,
   withHandlers({
     onLogout: () => () => firebase.auth().signOut()
